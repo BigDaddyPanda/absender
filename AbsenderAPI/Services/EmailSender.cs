0c84a42f2acfefb6d206ce858772df3e31b14f70
Yassin KISRAWI <yassinkisrawi42@gmail.com>
2018-10-30 00:22:58 +0100

    Adding Email Sender

--- AbsenderAPI/Services/EmailSender.cs
+++ AbsenderAPI/Services/EmailSender.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace AbsenderAPI.Services
@@ -9,8 +11,29 @@ namespace AbsenderAPI.Services
     // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
     public class EmailSender : IEmailSender
     {
+        private SmtpClient client;
+        private bool initialized;
+        private void InitializeConnexion()
+        {
+
+            client = new SmtpClient("smtp.gmail.com", 587);
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential("tekupabsender@gmail.com", "@Absender123");
+            initialized = true;
+
+
+        }
+
         public Task SendEmailAsync(string email, string subject, string message)
         {
+            if (!initialized) InitializeConnexion();
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress("tekupabsender@gmail.com");
+            mailMessage.To.Add(email);
+            mailMessage.Subject = subject;
+            mailMessage.Body = message;
+            client.Send(mailMessage);
             return Task.CompletedTask;
         }
     }
