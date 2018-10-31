8e90d86c60f1dd3051983961c89bbb874bff87ae
Yassin KISRAWI <yassinkisrawi42@gmail.com>
2018-10-31 09:14:38 +0100

    "Hot" Update

--- AbsenderAPI/Models/UniversityModels/JustificatifAbsence.cs
+++ AbsenderAPI/Models/UniversityModels/JustificatifAbsence.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,14 @@ namespace AbsenderAPI.Models.UniversityModels
 {
     public class JustificatifAbsence
     {
+        [Key]
+        public int IdJA{ get; set; }
+        public string PreviewJA { get; set; }
+        public bool EstAccepte { get; set; }
+
+        public int IdAbsence { get; set; }
+
+        [ForeignKey("IdAbsence")]
+        public Absence Absence { get; set; }
     }
 }
